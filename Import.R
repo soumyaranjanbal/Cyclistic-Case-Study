d12bc1bb38c9c3d097385ee802e5733fd7aa75dc
Soumyaranjan Bal <soumyaranjanbal4@gmail.com>
2023-10-21 22:13:02 +0200

    Create Import.R

---
+++ Import.R
@@ -0,0 +1,6 @@
+april_2023 <- read_csv("D:\\Users\\DELL\\Desktop\\PowerPoint\\Original Data\\
+                       April_2023_cyclistic_tripdata.csv")
+may_2023 <- read_csv("D:\\Users\\DELL\\Desktop\\PowerPoint\\Original Data\\
+                     May_2023_cyclistic_tripdata.csv")
+june_2023 <- read_csv("D:\\Users\\DELL\\Desktop\\PowerPoint\\Original Data\\
+                      June_2023_cyclistic_tripdata.csv")
